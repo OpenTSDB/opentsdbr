62f4745fc48555ac45e91e501a12bac01b2cc16f
David Holstius <david.holstius@gmail.com>
2013-02-03 15:58:12 -0800

    add tests for tsd_get()

--- inst/tests/test.tsd_get.r
+++ inst/tests/test.tsd_get.r
@@ -1,6 +1,6 @@
-context('parsing')
+context('tsd_get')
 
-test_that("parse_tags", {
+test_that("parse tags from string (ex: foo=bar baz=bap)", {
 	tag_keys <- c("serial", "host", "site")
 	tag_strings <- c(
 		"host=foo serial=bar site=bap",
@@ -16,7 +16,13 @@ myservice.latency.avg 1288900000 42 reqtype=foo abc=bap
 myservice.latency.avg 1288900000 51 reqtype=bar abc=bap
 "
 
-test_that("parse_content", {
-	dat <- parse_content(content, tags=c('reqtype', 'abc'))
-	#expect_equal(dat, data.frame())
+test_that("parse example content returned by TSD", {
+	tags <- c('reqtype', 'abc')
+	parsed <- parse_content(content, tags=tags)
+	expect_true(is.data.frame(parsed))
+	expect_equal(names(parsed)[1:3], c("metric", "timestamp", "value"))
+	expect_equal(names(parsed)[4:ncol(dat)], tags)
+	expect_true(is(parsed$timestamp, "POSIXct"))
+	expect_equivalent(attr(parsed$timestamp, "tzone"), Sys.timezone())
+	expect_equivalent(parsed$timestamp, Timestamp())
 })
