203002e14b5d1feb0ccc04d48659373270a4c11f
David Holstius <david.holstius@gmail.com>
2013-02-03 16:21:38 -0800

    do not export format.Timestamp

--- R/timestamps.r
+++ R/timestamps.r
@@ -1,10 +1,11 @@
-#' Timestamps with suitable default formatting and timezone specs
+#' Timestamps with suitable default formatting and timezone handling
 #'
 #' @note tz="" is equivalent to tz=Sys.timezone()
 #'
+#' @param	x		epoch time, character, or anything else convertable via \link{as.POSIXct}
 #' @param	tz		character string (see link{timezones})
 #' @param	origin	character string (see \link{as.POSIXct})
-#' @param	...		further arguments for, e.g., strptime() 
+#' @param	...		further arguments to be passed to or from other methods (example: format) 
 #' @export
 Timestamp <- function(x, tz, origin="1970-01-01 00:00:00", ...) {
 	if (is.numeric(x)) {
@@ -38,5 +39,4 @@ format_default <- function(x, tz="", ...) {
 	format.POSIXct(x, format="%Y-%m-%d %H:%M:%S %Z", tz=tz, usetz=FALSE)
 }
 
-#' @export
 format.Timestamp <- format_default
